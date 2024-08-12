323c9c62ce291acc099303cc8d43c622e46e8b06
marco <marcowang05@gmail.com>
2024-08-12 11:19:30 +0200

    unmodifiable groups

--- app/src/main/java/it/unitn/ds1/Messages/QuorumInfo.java
+++ app/src/main/java/it/unitn/ds1/Messages/QuorumInfo.java
@@ -3,12 +3,14 @@ package it.unitn.ds1.Messages;
 import java.io.Serializable;
 import java.util.List;
 import akka.actor.ActorRef;
+import java.util.Collections;
+import java.util.ArrayList;
 
 public class QuorumInfo implements Serializable {
     public final List<ActorRef> quorum;
 
     public QuorumInfo(List<ActorRef> quorum) {
-        this.quorum = quorum; // need to be unmodifiable and a copy of the list
+        this.quorum = Collections.unmodifiableList(new ArrayList<ActorRef>(quorum));
     }
 
 }
