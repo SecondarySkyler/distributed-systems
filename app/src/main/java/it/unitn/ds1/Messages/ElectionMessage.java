94ca7d4b1c0c8740995535af189962d7bb487d0e
Cristian <skylerhunter6@gmail.com>
2024-08-20 17:18:07 +0200

    fixed election message

--- app/src/main/java/it/unitn/ds1/Messages/ElectionMessage.java
+++ app/src/main/java/it/unitn/ds1/Messages/ElectionMessage.java
@@ -1,27 +1,32 @@
 package it.unitn.ds1.Messages;
 
 import java.io.Serializable;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 import akka.actor.ActorRef;
+import it.unitn.ds1.MessageIdentifier;
 
 public class ElectionMessage implements Serializable {
-    // need to be public final or private
-    public final HashMap<Integer, Integer> quorumState;
-    private int senderId;
-    private ActorRef from; // should we remove this? and use getSender() instead?
+    public final Map<Integer, MessageIdentifier> quorumState;
+    public final ActorRef from; 
 
-    public ElectionMessage(int id, int lastUpdate, ActorRef from) {
-        this.quorumState = new HashMap<>();
-        this.quorumState.put(id, lastUpdate);
-        this.senderId = id;
-        this.from = from;// to change using sender
+    public ElectionMessage(int id, MessageIdentifier lastUpdate, ActorRef from) {
+        HashMap<Integer, MessageIdentifier> temp = new HashMap<>();
+        temp.put(id, lastUpdate);
+        this.quorumState = Collections.unmodifiableMap(temp);
+        this.from = from;
     }
 
-    public void addState(int id, int lastUpdate) {
-        this.quorumState.put(id, lastUpdate);
+    public ElectionMessage(ActorRef from, HashMap<Integer, MessageIdentifier> quorumState) {
+        this.quorumState = Collections.unmodifiableMap(quorumState);
+        this.from = from;
     }
 
-    public ActorRef getSender() {
-        return this.from;
+    public ElectionMessage addState(int id, MessageIdentifier lastUpdate, ActorRef from, Map<Integer, MessageIdentifier> quorumState) {
+        HashMap<Integer, MessageIdentifier> temp = new HashMap<>(quorumState);
+        temp.put(id, lastUpdate);
+        return new ElectionMessage(from, temp);
     }
+
 }
