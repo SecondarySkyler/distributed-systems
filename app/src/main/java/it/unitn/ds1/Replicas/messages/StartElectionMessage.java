dd6514f7227dc558942df9258a81eca65c4fe51b
marco <marcowang05@gmail.com>
2024-12-02 23:08:04 +0100

    reason in election leader

--- app/src/main/java/it/unitn/ds1/Replicas/messages/StartElectionMessage.java
+++ app/src/main/java/it/unitn/ds1/Replicas/messages/StartElectionMessage.java
@@ -3,4 +3,9 @@ package it.unitn.ds1.Replicas.messages;
 import java.io.Serializable;
 
 public class StartElectionMessage implements Serializable {
+    public final String reason;
+
+    public StartElectionMessage(String reason) {
+        this.reason = reason;
+    }
 };
