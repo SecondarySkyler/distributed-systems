092026bc5164f49dc75accb9271ec4f55a42098b
SecondarySkyler <skylerhunter6@gmail.com>
2024-12-23 16:14:09 +0100

    Add comment

--- app/src/main/java/it/unitn/ds1/MessageIdentifier.java
+++ app/src/main/java/it/unitn/ds1/MessageIdentifier.java
@@ -2,28 +2,52 @@ package it.unitn.ds1;
 
 import java.util.Objects;
 
+/**
+ * A class to represent a message identifier, which is a pair of epoch and sequence number.
+ */
 public class MessageIdentifier implements Comparable<MessageIdentifier> {
     private int epoch;
     private int sequenceNumber;
 
+    /**
+     * Constructor
+     * @param epoch 
+     * @param sequenceNumber
+     */
     public MessageIdentifier(int epoch, int sequenceNumber) {
         this.epoch = epoch;
         this.sequenceNumber = sequenceNumber;
     }
 
+    /**
+     * Get the epoch
+     * @return epoch
+     */
     public int getEpoch() {
         return this.epoch;
     }
 
+    /**
+     * Get the sequence number
+     * @return sequence number
+     */
     public int getSequenceNumber() {
         return this.sequenceNumber;
     }
 
+    /**
+     * Increment the epoch
+     * @return new MessageIdentifier with incremented epoch
+     */
     public MessageIdentifier incrementEpoch() {
         // making it immutable
         return new MessageIdentifier(this.epoch + 1, 0);
     }
 
+    /**
+     * Increment the sequence number
+     * @return new MessageIdentifier with incremented sequence number
+     */
     public MessageIdentifier incrementSequenceNumber() {
         return new MessageIdentifier(this.epoch, this.sequenceNumber + 1);
     }
